ebbe0f862331789cb002a7def839d947b3e04b07
parth <parthbhatt268@gmail.com>
2023-02-28 21:03:29 +0530

    Added state to Profile tab

--- src/Component/Profile/profile.js
+++ src/Component/Profile/profile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './profile.css'
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
@@ -17,83 +17,119 @@ import { Container, FormControl, TextField, Typography } from '@mui/material';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import ResponsiveAppBar from '../Shared/NavBar';
 import NavBar from '../Shared/NavBar/navbar'
+import Button from '@mui/material/Button';
 
 
 function Profile() {
-    const [showPassword, setShowPassword] = React.useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [disEdit, setDisEdit] = useState(true);
+    const [profileData, setProfileData] = useState(
+        {
+            name: "Parth",
+            email: "hello@gmail.com",
+            // password: "",
+            address: "Navi Mumbai, India"
+        }
+    )
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleName = (e) => {
+        setProfileData(
+            {
+                ...profileData,
+                [e.target.name]: e.target.value
+            }
+        )
+    }
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+    const handleEditButtonToggle = () => {
+        setDisEdit(!disEdit)
+        let payload = {}
+        if (disEdit == false) {
+            payload = profileData
+        }
+        console.log("dekho", payload)
+    }
+    // const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    // const handleMouseDownPassword = (event) => {
+    //     event.preventDefault();
+    // };
     return (
         <>
-        <div className='profile_wrap'>
-            <div className='nav_bar_profile'>
-                Your Profile
-            </div>
-            <Paper
-                className='profile_img'
-                style={{
-                    padding: "20px",
-                    margin: "20px",
-                }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={4} xl={4}>
-                        <div
-                            className='center'
-                        >
-                            <Container>
-                                <Box
-                                    component="img"
-                                    sx={{
-                                        padding: "20px",
-                                        borderRadius: "1000px",
-                                        border: '5px solid #ff1200',
-                                        alignItems: 'center',
-                                        height: 222,
-                                        width: 222,
-                                        maxHeight: { xs: 222, md: 222 },
-                                        maxWidth: { xs: 222, md: 222 },
-                                    }}
-                                    alt="My image"
-                                    src="\Image\default_person.png"
-                                />
-                            </Container>
+            <div className='profile_wrap'>
+                <div className='nav_bar_profile'>
+                    Your Profile
+                </div>
+                <Paper
+                    className='profile_img'
+                    style={{
+                        padding: "20px",
+                        margin: "20px",
+                    }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={4} xl={4}>
+                            <div
+                                className='center'
+                            >
+                                <Container>
+                                    <Box
+                                        component="img"
+                                        sx={{
+                                            padding: "20px",
+                                            borderRadius: "1000px",
+                                            border: '5px solid #ff1200',
+                                            alignItems: 'center',
+                                            height: 222,
+                                            width: 222,
+                                            maxHeight: { xs: 222, md: 222 },
+                                            maxWidth: { xs: 222, md: 222 },
+                                        }}
+                                        alt="My image"
+                                        src="\Image\default_person.png"
+                                    />
+                                </Container>
 
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} md={8} xl={8}>
-                        <Paper
-                            style={{
-                                height: "60vh",
-                                padding: "20px",
-                                backgroundColor: "#ffefde"
-                            }}
-                        >
-                            <TextField
-                                required
-                                label="Name"
-                                variant='standard'
-                            />
-                            <br />
-                            <br />
-                            <TextField
-                                required
-                                label="Email"
-                                variant='standard'
-                            />
-                            <br />
-                            <br />
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} md={8} xl={8}>
+                            <Paper
+                                style={{
+                                    height: "70vh",
+                                    padding: "20px",
+                                    backgroundColor: "#ffefde"
+                                }}
+                            >
+                                <TextField
+                                    required
+                                    defaultValue={profileData.name}
+                                    disabled={disEdit}
+                                    name="name"
+                                    variant='standard'
+                                    onChange={(e) => { handleName(e) }}
+                                />
+                                <br />
+                                <br />
+                                <TextField
+                                    required
+                                    defaultValue={profileData.email}
+                                    disabled={disEdit}
+                                    name="email"
+                                    variant='standard'
+                                    onChange={(e) => { handleName(e) }}
+                                />
+                                <br />
+                                {/* <br />
                             <FormControl variant="standard">
                                 <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
                                 <Input
+                                value={profileData.password}
+                                disabled={disEdit}
                                     id="standard-adornment-password"
                                     type={showPassword ? 'text' : 'password'}
                                     endAdornment={
                                         <InputAdornment position="end">
                                             <IconButton
+                                            disabled={disEdit}
                                                 aria-label="toggle password visibility"
                                                 onClick={handleClickShowPassword}
                                                 onMouseDown={handleMouseDownPassword}
@@ -104,23 +140,37 @@ function Profile() {
                                     }
                                 />
                             </FormControl>
-                            <br />
-                            <br />
-                            <TextareaAutosize
-                                maxRows={6}
-                                aria-label="Message"
-                                placeholder="Address..."
-                                style={{
-                                    width: "280px",
-                                    minHeight: "100px",
-                                    backgroundColor: "transparent"
-                                }}
-                            />
-                        </Paper>
+                            <br /> */}
+                                <br />
+                                <TextareaAutosize
+                                    defaultValue={profileData.address}
+                                    name="address"
+                                    disabled={disEdit}
+                                    onChange={(e) => {handleName(e)}}
+                                    maxRows={6}
+                                    aria-label="Message"
+                                    placeholder="Address..."
+                                    style={{
+                                        width: "250px",
+                                        minHeight: "100px",
+                                        backgroundColor: "transparent"
+                                    }}
+                                />
+                                <br />
+                                <Button
+                                    variant='contained'
+                                    onClick={handleEditButtonToggle}
+                                    style={{
+                                        backgroundColor: "orange"
+                                    }}
+                                >
+                                    {disEdit ? "Edit" : "Save"}
+                                </Button>
+                            </Paper>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Paper>
-        </div>
+                </Paper>
+            </div>
         </>
     )
 }
