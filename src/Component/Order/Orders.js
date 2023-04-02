fe581d79c90fa8b2decadd4846cf4d91cba31dc8
Rupali Singh <rupalisingh2304@gmail.com>
2023-04-02 19:06:17 +0530

    stripe checkout

--- src/Component/Order/Orders.js
+++ src/Component/Order/Orders.js
@@ -1,15 +1,31 @@
-import { Button, Grid } from '@mui/material'
-import React, { useState } from 'react'
-import { styled } from '@mui/material/styles';
-import {Table, TableBody, TableContainer, TableHead, TableRow, Paper, Radio, RadioGroup, 
-  FormControlLabel, FormControl, FormLabel, Card, CardMedia} from '@mui/material';
-  import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import './style.css';
-import { connect } from "react-redux"
-import { useEffect } from 'react';
-import NoData from '../Shared/NoData';
+import { Button, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import {
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Card,
+  CardMedia,
+} from "@mui/material";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import "./style.css";
+import { connect } from "react-redux";
+import { useNavigate  } from "react-router-dom";
+import { useEffect } from "react";
+import NoData from "../Shared/NoData";
 import cartLogo from "../../Image/logo.png";
-import {postOrderDetailPayment} from "../../Store/AsyncThunk/orderAsync"
+import { postOrderDetailPayment } from "../../Store/AsyncThunk/orderAsync";
+import { loadStripe } from "@stripe/stripe-js";
+
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -22,62 +38,119 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: "#fff4f1",
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
-
-
 const Orders = (props) => {
+  const navigate = useNavigate();
+  const publishKey = "pk_test_51MqHSBSGD0NxWjnWu2dT5naINF4Qzc9FzmgnDmbkUBQrgxfUeXL28om4lMoalOElZta7TiHAnQ740nE1beSN31sM00tnoi6ny2"
+  
   const [orderList, setOrderList] = React.useState();
   const [billAmt, setBillAmt] = React.useState();
   const [finAmt, setFinAmt] = React.useState();
 
-
   useEffect(() => {
     let a = 0;
-    props.savedDish.length > 0 ? props.savedDish.map(row => {
-      a = row.p_amount + a
-    }) : ""
-    setBillAmt(a)
-    setFinAmt(a - a * (25 / 100))
-  }, [])
+    props.savedDish.length > 0
+      ? props.savedDish.map((row) => {
+          a = row.p_amount + a;
+        })
+      : "";
+    setBillAmt(a);
+    setFinAmt(a - a * (25 / 100));
+  }, []);
 
-  const handlePayment = () => {
-    let payload = {}
-    payload.custId = "12345678" //filhal hard coded hai boz phele hummne login or register pe local me custId staore karna hoga fir next time se yaha se custID fetch karke yaha send karenge
-    payload.orderDetail = props.savedDish
-    console.log("aya kya ander")
-    props.postOrderDetailPayment(payload)
-
-  }
+  const handlePayment = async() => {
+    let payload = {};
+    let custId = localStorage.getItem("customer_id")
+    payload.custId = custId;
+    payload.orderDetail = props.savedDish;
+   // navigate('/paymentsGateway',{state:{orderDetail: payload.orderDetail}}) 
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    switch (new Date().getMonth() + 1) {
+      case 1:
+        month = "January";
+        break;
+      case 2:
+        month = "February";
+        break;
+      case 3:
+        month = "March";
+        break;
+      case 4:
+        month = "April";
+        break;
+      case 5:
+        month = "May";
+        break;
+      case 6:
+        month = "June";
+        break;
+      case 7:
+        month = "July";
+        break;
+      case 8:
+        month = "August";
+        break;
+      case 9:
+        month = "September";
+        break;
+      case 10:
+        month = "October";
+        break;
+      case 11:
+        month = "November";
+        break;
+      case 12 :
+        month = "December";
+    }
+    let currentDate = `${day}-${month}-${year}`;
+    payload.orderDate = currentDate   
+    const stripe = await loadStripe(publishKey); 
+    console.log(payload)
+    props.postOrderDetailPayment(payload).
+    unwrap()
+    .then(async(response) => {
+      const result = await stripe.redirectToCheckout({ 
+        sessionId: response.id, 
+      });
+      if (result.error) { 
+        console.log(result.error); 
+      } 
+    });
+  };
   return (
-
     <>
-      <div className='Order'>
-
+      <div className="Order">
         <Paper
           sx={{
-            padding: "10px 10px 10px 10px"
+            padding: "10px 10px 10px 10px",
           }}
         >
           <Grid container spacing={2}>
             <Grid item xs={10} md={10} lg={11}>
               <Paper
-              elevation={0}
+                elevation={0}
                 sx={{
                   height: "45px",
                   textAlign: "start",
                   padding: "12px 0px 0px 5px",
                   margin: "0px",
                   fontSize: "20px",
-                  background: "transperant"
-                }}>My Cart</Paper>
+                  background: "transperant",
+                }}
+              >
+                My Cart
+              </Paper>
             </Grid>
             <Grid item xs={2} md={2} lg={1}>
               <Card>
@@ -86,7 +159,7 @@ const Orders = (props) => {
                   image={cartLogo}
                   alt="green"
                   height="50px"
-                //width="400px"
+                  //width="400px"
                 />
               </Card>
             </Grid>
@@ -96,10 +169,9 @@ const Orders = (props) => {
           sx={{
             padding: "10px 15px 10px 15px",
             borderRadius: "10px",
-            marginRight: "20px"
-
-
-          }}>
+            marginRight: "20px",
+          }}
+        >
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 100 }} aria-label="customized table">
               <TableHead>
@@ -111,26 +183,22 @@ const Orders = (props) => {
                 </TableRow>
               </TableHead>
               <TableBody>
-
-                {console.log("hhh", props.savedDish, props.loginStatus)}{
-                  props.savedDish.length > 0 ? props.savedDish.map(row => {
+                {props.savedDish.length > 0 ? (
+                  props.savedDish.map((row) => {
                     return (
-                      <StyledTableRow >
+                      <StyledTableRow>
                         <StyledTableCell component="th" scope="row">
                           {row.p_name}
                         </StyledTableCell>
-                        <StyledTableCell >₹ {row.p_price}
-                        </StyledTableCell>
-                        <StyledTableCell>{row.p_itemCount}
-                        </StyledTableCell>
-                        <StyledTableCell>₹ {row.p_amount}
-                        </StyledTableCell>
+                        <StyledTableCell>₹ {row.p_price}</StyledTableCell>
+                        <StyledTableCell>{row.p_itemCount}</StyledTableCell>
+                        <StyledTableCell>₹ {row.p_amount}</StyledTableCell>
                       </StyledTableRow>
-                    )
-                  }) : <NoData />
-                }
-
-
+                    );
+                  })
+                ) : (
+                  <NoData />
+                )}
               </TableBody>
             </Table>
           </TableContainer>
@@ -139,10 +207,9 @@ const Orders = (props) => {
           sx={{
             padding: "10px 15px 10px 15px",
             borderRadius: "10px",
-            marginRight: "20px"
-
-
-          }}>
+            marginRight: "20px",
+          }}
+        >
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 100 }} aria-label="customized table">
               <TableHead>
@@ -151,27 +218,27 @@ const Orders = (props) => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {
-                  billAmt ?
-                    <>
-                      <StyledTableRow >
-                        <StyledTableCell component="th" scope="row">
-                          Total Amount - {billAmt}
-                        </StyledTableCell>
-                      </StyledTableRow>
-                      <StyledTableRow >
-                        <StyledTableCell component="th" scope="row">
-                          Discount - 25%
-                        </StyledTableCell>
-                      </StyledTableRow>
-                      <StyledTableRow >
-                        <StyledTableCell component="th" scope="row">
-                          Final Amount - {finAmt}
-                        </StyledTableCell>
-                      </StyledTableRow>
-                    </>
-                    : <NoData />
-                }
+                {billAmt ? (
+                  <>
+                    <StyledTableRow>
+                      <StyledTableCell component="th" scope="row">
+                        Total Amount - {billAmt}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                      <StyledTableCell component="th" scope="row">
+                        Discount - 25%
+                      </StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                      <StyledTableCell component="th" scope="row">
+                        Final Amount - {finAmt}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  </>
+                ) : (
+                  <NoData />
+                )}
               </TableBody>
             </Table>
           </TableContainer>
@@ -181,9 +248,9 @@ const Orders = (props) => {
           sx={{
             padding: "10px 15px 10px 15px",
             borderRadius: "10px",
-            marginRight: "20px"
-
-          }}>
+            marginRight: "20px",
+          }}
+        >
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 100 }} aria-label="customized table">
               <TableHead>
@@ -192,78 +259,88 @@ const Orders = (props) => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {
-                  billAmt ?
-                    <>
-                      <StyledTableRow >
-                        <Paper
-                          sx={{
-                            padding: "10px"
-                          }}>
-                          <FormControl>
-                            <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
-                            <RadioGroup
-                              aria-labelledby="demo-radio-buttons-group-label"
-                              defaultValue="Cash On Delivery"
-                              name="radio-buttons-group"
-                            >
-                              <FormControlLabel value="female" control={<Radio />} label="Cash On Delivery" />
-                              <FormControlLabel value="male" disabled control={<Radio />} label="Online Payment (Not available for your Location)" />
-                              <FormControlLabel value="other" disabled control={<Radio />} label="Credit/Debit Card (Not available for your Location)" />
-                            </RadioGroup>
-                          </FormControl>
-                        </Paper>
-                      </StyledTableRow>
-                    </>
-                    : <NoData />
-                }
+                {billAmt ? (
+                  <>
+                    <StyledTableRow>
+                      <Paper
+                        sx={{
+                          padding: "10px",
+                        }}
+                      >
+                        <FormControl>
+                          <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
+                          <RadioGroup
+                            aria-labelledby="demo-radio-buttons-group-label"
+                            defaultValue="Cash On Delivery"
+                            name="radio-buttons-group"
+                          >
+                            <FormControlLabel
+                              value="female"
+                              control={<Radio />}
+                              label="Cash On Delivery"
+                            />
+                            <FormControlLabel
+                              value="male"
+                              disabled
+                              control={<Radio />}
+                              label="Online Payment (Not available for your Location)"
+                            />
+                            <FormControlLabel
+                              value="other"
+                              disabled
+                              control={<Radio />}
+                              label="Credit/Debit Card (Not available for your Location)"
+                            />
+                          </RadioGroup>
+                        </FormControl>
+                      </Paper>
+                    </StyledTableRow>
+                  </>
+                ) : (
+                  <NoData />
+                )}
               </TableBody>
             </Table>
           </TableContainer>
         </Grid>
 
-
-
-        <div 
-        className='footer_bar_orders'
-        style={{
-
-          //backgroundColor: "orange",
-          //borderTop:"2px solid red",
-         
-
-        }}>
-
+        <div
+          className="footer_bar_orders"
+          style={
+            {
+              //backgroundColor: "orange",
+              //borderTop:"2px solid red",
+            }
+          }
+        >
           <Button
-            variant="contained" color="success"
+            variant="contained"
+            color="success"
             onClick={handlePayment}
             sx={{
               padding: "10px",
-              margin: "10px"
+              margin: "10px",
               //margin: "2% 10% 2% 10%"
-            }}>
-            Pay Amount: {finAmt > 0 ? finAmt : 0}/- {finAmt > 0 ? "& Place Order" : ""}
+            }}
+          >
+            Pay Amount: {finAmt > 0 ? finAmt : 0}/-{" "}
+            {finAmt > 0 ? "& Place Order" : ""}
           </Button>
         </div>
-
-
-
-      </div >
+      </div>
     </>
-
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
     savedDish: state.savedDish,
-    loginStatus: state.loginStatus
+    loginStatus: state.loginStatus,
   };
 };
 
 const mapDispatchToProps = {
-  postOrderDetailPayment
-
+  postOrderDetailPayment,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Orders);
