7849dee1200bc65912ae09c8f8eaaa1cadf4c5c7
parth <parthbhatt268@gmail.com>
2023-04-09 00:56:53 +0530

    About Page improvement

--- src/Component/About/AboutPerformance.js
+++ src/Component/About/AboutPerformance.js
@@ -6,27 +6,27 @@ const AboutPerformance = () => {
       <div class="row1-container">
         <div class="box box-down cyan">
           <h2>3 Millions+ Downloads</h2>
-          <p>
-            Downloads on iOS AppStore and Android PlayStore.</p>
+          {/* <p>
+            Downloads on iOS AppStore and Android PlayStore.</p> */}
           <img src="https://assets.codepen.io/2301174/icon-supervisor.svg" alt="" />
         </div>
 
         <div class="box red">
           <h2>1000+ Home Delivery per HJours</h2>
-          <p>We have Delivering in 30+ Major Cities</p>
+          {/* <p>We have Delivering in 30+ Major Cities</p> */}
           <img src="https://assets.codepen.io/2301174/icon-team-builder.svg" alt="" />
         </div>
 
         <div class="box box-down blue">
           <h2>9000+ Hotels have paterend</h2>
-          <p>We have partnered with differnet Hotels to bring differnet cusine to customer by just ordering through app</p>
+          {/* <p>We have partnered with differnet Hotels to bring differnet cusine to customer by just ordering through app</p> */}
           <img src="https://assets.codepen.io/2301174/icon-calculator.svg" alt="" />
         </div>
       </div>
       <div class="row2-container">
         <div class="box orange">
           <h2>1600000+ Meals Delivered </h2>
-          <p>We have been delivering food as reasonable price.</p>
+          {/* <p>We have been delivering food as reasonable price.</p> */}
           <img src="https://assets.codepen.io/2301174/icon-karma.svg" alt="" />
         </div>
       </div>
